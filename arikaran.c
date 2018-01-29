21bc51328093419236005e85188b6b0be26cecfa
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-01-29 13:52:17 +0530

    Create arikaran.c

---
+++ arikaran.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ clrscr();
+ printf("enter the values:");
+ scanf("%d",&a);
+ c=a+b;
+ printf("add the two numbers:");
+ scanf("%d",&c);
+ getch();
+ }
+
